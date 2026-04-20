896c686ea67821580dc57476758037ebda78774e
LEONEL ALEJANDRO MOREIRA RENGIFO <leocuypulga10@gmail.com>
2026-04-19 22:43:13 -0500

    Ejercicio7

--- Moreira_Leonel_Tarea individual/src/Ejercicio7.java
+++ Moreira_Leonel_Tarea individual/src/Ejercicio7.java
@@ -1,9 +1,26 @@
-public class Ejercicio7 {
+public class Main {
     public static void main(String[] args) {
-        InventarioProducto p = new InventarioProducto("PRD-105","Disco SSD 1TB","Almacenamiento",89.50, 12, 8);
-        p.setPrecio(95.00);
-        p.setStock(5);
-        p.setStock(-3);
+
+        Producto p = new Producto(
+                "PRD-105",
+                "Disco SSD 1TB",
+                "Almacenamiento",
+                89.50,
+                12,
+                8
+        );
+
+        p.mostrarReporte();
+
+        System.out.println("\n--- ACTUALIZACIÓN ---");
+        p.actualizarPrecio(95.00);
+        p.actualizarStock(5);
+
+        p.mostrarReporte();
+
+        System.out.println("\n--- CASO INVÁLIDO ---");
+        p.actualizarStock(-3);
+
         p.mostrarReporte();
     }
 }
